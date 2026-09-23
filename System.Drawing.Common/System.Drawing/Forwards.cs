05b1cb20ebcec61aa6133a3a14d7bb8c74b3df8b
Holger Eiboeck <holger@eiboeck.de>
2026-09-23 09:47:27 +0200

    Forward drawing primitives for API compatibility

--- System.Drawing.Common/System.Drawing/Forwards.cs
+++ System.Drawing.Common/System.Drawing/Forwards.cs
@@ -2,5 +2,14 @@ using System;
 using System.Drawing;
 using System.Runtime.CompilerServices;
 
+[assembly: TypeForwardedTo(typeof(Color))]
+[assembly: TypeForwardedTo(typeof(KnownColor))]
+[assembly: TypeForwardedTo(typeof(Point))]
+[assembly: TypeForwardedTo(typeof(PointF))]
+[assembly: TypeForwardedTo(typeof(Rectangle))]
+[assembly: TypeForwardedTo(typeof(RectangleF))]
+[assembly: TypeForwardedTo(typeof(Size))]
+[assembly: TypeForwardedTo(typeof(SizeF))]
+
 [assembly: TypeForwardedTo(typeof(ColorTranslator))]
 [assembly: TypeForwardedTo(typeof(SystemColors))]
